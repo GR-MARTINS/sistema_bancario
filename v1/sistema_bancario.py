2f341a161b51f9aae788986dbfee1a1b44906478
Glayton <gr.martins@outlook.com>
2025-08-19 17:05:40 -0300

    implementacao da operacao sacar

--- v1/sistema_bancario.py
+++ v1/sistema_bancario.py
@@ -26,6 +26,35 @@ while True:
 
         else: 
             print("Não foi possível realizar o depósito. Valor inválido!")
+            
+    elif opcao == "s":
+        valor = float(input("Digite o valor que você deseja sacar: "))
+
+        excedeu_saldo = valor > saldo
+        
+        excedeu_limite = valor > limite
+        
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        
+        if excedeu_saldo:
+            print("Não foi possível realizar o saque. Você não possui saldo suficiente")
+            
+        elif excedeu_limite:
+            print("Não foi possível realizar o saque. O valor do saque excede o limite da conta.")
+            
+        elif excedeu_saques:
+            print("Não foi possível realizar o saque. Numero máximo de saques excedidos.")
+        
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque:    R$ {valor:.2}"
+            numero_saques += 1
+        
+        else:
+            print("Não foi possível realizar o saque. O valor informado é invalido.")
+            
+    elif opcao == "q":
+        break
 
     else:
         print("Operação inválida! Por favor, seleciona novamente a operação desejada.")
