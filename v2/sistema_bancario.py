b00cb18709571c41e66ff02f3cc48bbe6fa460bb
Glayton <gr.martins@outlook.com>
2025-08-20 12:32:54 -0300

    implementacao das funcoes menu e main

---
+++ v2/sistema_bancario.py
@@ -0,0 +1,79 @@
+def menu():
+    menu = """
+    Selecione uma das opções abaixo:
+    
+     [d] Depositar
+     [s] Sacar
+     [e] Extrato
+    [nu] Novo Usuário
+    [lu] Listar Usuários
+    [nc] Nova conta
+    [lc] Listar Contas
+     [q] Sair
+
+    =>"""
+    return input(menu)
+
+
+def main():
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    LIMITE_SAQUES = 3
+
+    while True:
+        
+        opcao = menu()
+        
+        if opcao == "d":
+            valor = float(input("Digite o valor que você deseja depositar: "))
+            
+            if valor > 0:
+                saldo += valor
+                extrato += f"Depósito: R$ {valor:.2}\n"
+
+            else: 
+                print("Não foi possível realizar o depósito. Valor inválido!")
+                
+        elif opcao == "s":
+            valor = float(input("Digite o valor que você deseja sacar: "))
+
+            excedeu_saldo = valor > saldo
+            
+            excedeu_limite = valor > limite
+            
+            excedeu_saques = numero_saques >= LIMITE_SAQUES
+            
+            if excedeu_saldo:
+                print("Não foi possível realizar o saque. Você não possui saldo suficiente")
+                
+            elif excedeu_limite:
+                print("Não foi possível realizar o saque. O valor do saque excede o limite da conta.")
+                
+            elif excedeu_saques:
+                print("Não foi possível realizar o saque. Numero máximo de saques excedidos.")
+            
+            elif valor > 0:
+                saldo -= valor
+                extrato += f"Saque:    R$ {valor:.2}"
+                numero_saques += 1
+            
+            else:
+                print("Não foi possível realizar o saque. O valor informado é invalido.")
+                
+        elif opcao == "e":
+            print("\n=================EXTRATO=================")
+            print("Até o momento, não houve nenhuma movimentação na conta" if not extrato else extrato)
+            print(f"\nSaldo: R$ {saldo:.2}")
+            print("\n=========================================")
+            
+        elif opcao == "q":
+            break
+
+        else:
+            print("Operação inválida! Por favor, seleciona novamente a operação desejada.")
+            
+
+if __name__ == "__main__":
+    main() 
