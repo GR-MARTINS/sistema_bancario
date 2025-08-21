5ad7100cc83b8ddd1e45ffc5439fbde57dd01343
Glayton <gr.martins@outlook.com>
2025-08-21 18:25:08 -0300

    impletentacao das classes da UML

--- v3/sistema_bancario.py
+++ v3/sistema_bancario.py
@@ -1,3 +1,216 @@
+import textwrap
+from abc import ABC, abstractmethod
+from datetime import date, datetime
+
+class Cliente:
+    def __init__(self, endereco: str):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta: "Conta", transacao: "Transacao"):
+        transacao.registrar(conta)
+
+    def adicionar_conta(self, conta:"Conta"):
+        self.contas.append(conta)
+        
+    def __repr__(self):
+        atributos = []
+        
+        for chave, valor in self.__dict__.items():
+            if isinstance(valor, list):
+                atributos.append(f"{chave}=[...]")  # evita poluição
+                
+            else:
+                atributos.append(f"{chave}={valor!r}")
+                
+        return f"{self.__class__.__name__}({', '.join(atributos)})"
+
+
+class PessoaFisica(Cliente):
+    def __init__(self, cpf:int, nome: str, endereco:str, data_nascimento: date):
+        super().__init__(endereco)
+        self.cpf = cpf
+        self.nome = nome
+        self.endereco = endereco
+        self.data_nascimento = data_nascimento
+
+
+class Conta:
+    def __init__(self, numero_da_conta: int, cliente: Cliente):
+        self._saldo = 0
+        self._numero = numero_da_conta
+        self._agencia = "0001"
+        self._cliente = cliente
+        self._historico = Historico()
+        
+    @classmethod
+    def nova_conta(cls, numero:int, cliente: Cliente):
+        return cls(numero, cliente)
+    
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+
+    @property
+    def historico(self):
+        return self._historico
+        
+    def sacar(self, valor: int):
+        saldo = self.saldo
+        excedeu_saldo = valor > saldo
+    
+        if excedeu_saldo:
+            print("\nNão foi possível realizar o saque. Você não possui saldo suficiente")
+            
+        elif valor > 0:
+            self._saldo -= valor
+            print(f"\nSaque no valor de R$ {valor:.2f} realizado com sucesso!")
+            return True
+        
+        else:
+            print("\nNão foi possível realizar o saque. Valor inválido!")
+            
+        return False
+    
+    def depositar(self, valor: int):
+        if valor > 0:
+            self._saldo += valor
+            print(f"\nDepósito no valor de R$ {valor:.2f} realizado com sucesso!")
+        
+        else:
+            print("\nNão foi possível realizar o depósito. Valor inválido!")
+            return False
+        
+        return True
+
+    def __repr__(self):
+        atributos = []
+        
+        for chave, valor in self.__dict__.items():
+            if isinstance(valor, list):
+                atributos.append(f"{chave}=[...]")  # evita poluição
+                
+            else:
+                atributos.append(f"{chave}={valor!r}")
+                
+        return f"{self.__class__.__name__}({', '.join(atributos)})"
+       
+        
+class ContaCorrente(Conta):
+    def __init__(self, numero: int, cliente: Cliente, limite=500, limite_saque=3):
+        super().__init__(numero, cliente)
+        self.limite = limite
+        self.limite_saques = limite_saque
+        
+    def sacar(self, valor: int):
+        numero_saque = len(
+            [
+                transacao for transacao in self.historico.transacoes if transacao["tipo"] == Saque.__name__
+            ]
+        )
+    
+        excedeu_limite = valor > self.limite
+        excedeu_saques = numero_saque >= self.limite_saques
+        
+        if excedeu_limite:
+            print("\nNão foi possível realizar o saque. O valor do saque excede o limite da conta.")
+            
+        elif excedeu_saques:
+            print("\nNão foi possível realizar o saque. Numero máximo de saques excedidos.")
+    
+        else:
+            return super().sacar(valor)
+        
+        return False
+       
+        
+class Historico:
+    def __init__(self):
+        self._transacoes = []
+
+    @property
+    def transacoes(self):
+        return self._transacoes
+    
+    @transacoes.setter
+    def transacoes(self, transacao: "Transacao"):
+        self._transacoes.append(transacao)
+        
+    def adicionar_transacao(self, transacao: "Transacao"):
+        self.transacoes = {
+            "tipo": transacao.__class__.__name__,
+            "valor": transacao.valor,
+            "data": datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+        }
+        
+    def __repr__(self):
+        atributos = []
+        
+        for chave, valor in self.__dict__.items():
+            if isinstance(valor, list):
+                atributos.append(f"{chave}=[...]")  # evita poluição
+                
+            else:
+                atributos.append(f"{chave}={valor!r}")
+                
+        return f"{self.__class__.__name__}({', '.join(atributos)})"
+    
+    
+# interface = classe abstrata
+class Transacao(ABC):
+    @property
+    @abstractmethod
+    def valor(self):
+        pass
+    
+    @classmethod
+    @abstractmethod
+    def registrar(cls, conta: Conta):
+        pass
+    
+    
+class Saque(Transacao):
+    def __init__(self, valor: int):
+        self._valor = valor
+    
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta: Conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+    
+            
+class Deposito(Transacao):
+    def __init__(self, valor:int):
+        self._valor = valor
+    
+    @property   
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta: Conta):
+        sucesso_transacao = conta.depositar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+            
+
 def menu():
     menu = """
     Selecione uma das opções abaixo:
